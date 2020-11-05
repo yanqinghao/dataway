217c428c14502dcc514d38ef2e8bc2027ac2bb43
YanQinghao <woshiyanqinghao@gmail.com>
2020-11-05 16:31:36 +0800

    fix docker tools

--- tools/build.sh
+++ tools/build.sh
@@ -3,14 +3,14 @@
 set -e
 
 VERSION=$(bash tools/version.sh)
-DOCKERBASE="maven"
+DOCKERBASE="registry-vpc.cn-shanghai.aliyuncs.com/shuzhi-amd64/suanpan-hasor-base"
 if [[ $1 == "master" ]]; then
-    echo "build from maven:3-openjdk-11"
+    echo "build from suanpan-hasor-base"
     echo "build suanpan-dataway:latest and ${VERSION}"
     BUILD_VERSIONS=${VERSION}
     BUILD_TAGS="latest"
 else
-    echo "build from maven:3-openjdk-11"
+    echo "build from suanpan-hasor-base"
     echo "build suanpan-dataway:preview and preview-${VERSION}"
     BUILD_VERSIONS="preview-${VERSION}"
     BUILD_TAGS="preview"
